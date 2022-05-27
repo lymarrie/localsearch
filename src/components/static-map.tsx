e0e7904847a48d8d5ca4c990421d05f629c350e1
lymarrie <lucmarrie1@gmail.com>
2022-05-27 16:28:03 -0400

    map id

--- src/components/static-map.tsx
+++ src/components/static-map.tsx
@@ -11,7 +11,7 @@ const StaticMap = (props: Coordinates) => {
   return (
     <>
       <div className="section flex flex-col items-center justify-center gap-y-10 pb-12">
-        <h2 className="text-4xl">Map</h2>
+        <h2 id="map" className="text-4xl">Map</h2>
         <img
           className="w-11/12 rounded-xl drop-shadow-md sm:w-3/4 "
           width="300"
